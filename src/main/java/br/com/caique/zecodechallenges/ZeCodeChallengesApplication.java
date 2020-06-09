d612a61fe358d9973ec649a539d32d31962826e1
caiquecointrade <caique@cointrade.cx>
2020-06-09 01:23:52 -0300

    Adicionando a estrutura do projeto

---
+++ src/main/java/br/com/caique/zecodechallenges/ZeCodeChallengesApplication.java
@@ -0,0 +1,13 @@
+package br.com.caique.zecodechallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeCodeChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeCodeChallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caique/zecodechallenges/ZeCodeChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caique.zecodechallenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeCodeChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
